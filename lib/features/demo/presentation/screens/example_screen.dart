8f44743b9860a86a95e48928107902501c306ca4
Vũ Tuấn Anh <tanhvta@gmail.com>
2023-11-23 09:31:42 +0700

    add flutter eval

--- lib/features/demo/presentation/screens/example_screen.dart
+++ lib/features/demo/presentation/screens/example_screen.dart
@@ -1,218 +1,102 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:file_picker/file_picker.dart';
+import 'package:dart_eval/dart_eval_bridge.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:restart_app/restart_app.dart';
-import 'package:shorebird_code_push/shorebird_code_push.dart';
-
-final _shorebirdCodePush = ShorebirdCodePush();
+import 'package:flutter_eval/flutter_eval.dart';
+import 'package:flutter_eval/widgets.dart';
 
 @RoutePage(name: 'ExampleScreen')
-class ExampleScreen extends StatefulWidget {
+class ExampleScreen extends StatelessWidget {
   const ExampleScreen({super.key});
 
   @override
-  State<ExampleScreen> createState() => _ExampleScreenState();
+  Widget build(BuildContext context) {
+    // Load a potential hot-swap update to be used by our app. If the URI responds with
+    // an error, it will be treated as no update present.
+    return HotSwapLoader(
+        // Updates can be loaded from the network using http:// and https://,
+        // or from local files/Flutter assets using file:// and asset:// respectively.
+        uri: 'https://storage.googleapis.com/eval-files/update4.evc',
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        ));
+  }
 }
 
-class _ExampleScreenState extends State<ExampleScreen> {
-  final _isShorebirdAvailable = _shorebirdCodePush.isShorebirdAvailable();
-  int? _currentPatchVersion;
-  bool _isCheckingForUpdate = false;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-  /// sẽ có các case cần phải check:
-  /// - thay đổi logic/UI (new patch)
-  /// - thay đổi thư viện (new release)
-  /// - thay đổi quyền truy cập của android/ios (new release)
-  /// - thay đổi cấu hình của android/android manifest (new release)
+  final String title;
 
   @override
-  void initState() {
-    super.initState();
-    // Request the current patch number.
-    _shorebirdCodePush.currentPatchNumber().then((currentPatchVersion) {
-      if (!mounted) return;
-      setState(() {
-        _currentPatchVersion = currentPatchVersion;
-      });
-    });
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  Future<void> _checkForUpdate() async {
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
     setState(() {
-      _isCheckingForUpdate = true;
+      _counter++;
     });
-
-    // Ask the Shorebird servers if there is a new patch available.
-    final isUpdateAvailable =
-        await _shorebirdCodePush.isNewPatchAvailableForDownload();
-
-    if (!mounted) return;
-
-    setState(() {
-      _isCheckingForUpdate = false;
-    });
-
-    if (isUpdateAvailable) {
-      _showUpdateAvailableBanner();
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('No update available'),
-        ),
-      );
-    }
-  }
-
-  void _showDownloadingBanner() {
-    ScaffoldMessenger.of(context).showMaterialBanner(
-      const MaterialBanner(
-        content: Text('Downloading...'),
-        actions: [
-          SizedBox(
-            height: 14,
-            width: 14,
-            child: CircularProgressIndicator(
-              strokeWidth: 2,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _showUpdateAvailableBanner() {
-    ScaffoldMessenger.of(context).showMaterialBanner(
-      MaterialBanner(
-        content: const Text('Update available'),
-        actions: [
-          TextButton(
-            onPressed: () async {
-              ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
-              await _downloadUpdate();
-
-              if (!mounted) return;
-              ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
-            },
-            child: const Text('Download'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _showRestartBanner() {
-    ScaffoldMessenger.of(context).showMaterialBanner(
-      const MaterialBanner(
-        content: Text('A new patch is ready!'),
-        actions: [
-          TextButton(
-            // Restart the app for the new patch to take effect.
-            onPressed: Restart.restartApp,
-            child: Text('Restart app'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> _downloadUpdate() async {
-    _showDownloadingBanner();
-
-    await Future.wait([
-      _shorebirdCodePush.downloadUpdateIfAvailable(),
-      // Add an artificial delay so the banner has enough time to animate in.
-      Future<void>.delayed(const Duration(milliseconds: 250)),
-    ]);
-
-    if (!mounted) return;
-
-    ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
-    _showRestartBanner();
   }
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final heading = _currentPatchVersion != null
-        ? '$_currentPatchVersion'
-        : 'No patch installed';
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: theme.colorScheme.inversePrimary,
-        title: const Text('Shorebird with Flutter'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Hế lô patch mới nè'),
-            const Text('Current patch version:'),
-            Text(
-              heading,
-              style: theme.textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 20),
-            if (!_isShorebirdAvailable)
+    // This method is hot-swappable with flutter_eval.
+    return HotSwap(
+      // To swap it out, we'll need an ID. In the update we'll compile with the
+      // dart_eval CLI, this will be mapped to a function with the annotation
+      // @RuntimeOverride('#main_scaffold_1')
+      id: '#main_scaffold_1',
+
+      // Args that may be passed to the hot-swapped method must be wrapped
+      // in a dart_eval wrapper, such as $BuildContext.
+      // Remember that the hot-swapped method doesn't have access to any of our
+      // app's state on its own. To allow it to access fields from the current
+      // widget's state or any data, it must be passed in as a function argument.
+      args: [
+        $BuildContext.wrap(context),
+        _counter, // ints, doubles, bools, and Lists are passed without wrapping
+        $Function((runtime, target, args) {
+          _incrementCounter();
+          return null;
+        })
+      ],
+
+      // If there is no override present, HotSwap will execute the childBuilder,
+      // ensuring minimal impact to performance.
+      childBuilder: (context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
               Text(
-                'Shorebird Engine not available.',
-                style: theme.textTheme.bodyLarge?.copyWith(
-                  color: theme.colorScheme.error,
-                ),
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
               ),
-            if (_isShorebirdAvailable)
-              ElevatedButton(
-                onPressed: _isCheckingForUpdate ? null : _checkForUpdate,
-                child: _isCheckingForUpdate
-                    ? const _LoadingIndicator()
-                    : const Text('Check for update'),
-              ),
-            ElevatedButton(
-              onPressed: () {
-                Fluttertoast.showToast(
-                    msg: "This is Center Short Toast",
-                    toastLength: Toast.LENGTH_SHORT,
-                    gravity: ToastGravity.CENTER,
-                    timeInSecForIosWeb: 1,
-                    backgroundColor: Colors.red,
-                    textColor: Colors.white,
-                    fontSize: 16.0);
-              },
-              child: const Text('Show toast'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                FilePickerResult? result =
-                    await FilePicker.platform.pickFiles();
-
-                if (result != null) {
-                  PlatformFile file = result.files.first;
-
-                  print(file.name);
-                  print(file.bytes);
-                  print(file.size);
-                  print(file.extension);
-                  print(file.path);
-                }
-              },
-              child: const Text('Pick file'),
-            )
-          ],
+            ],
+          ),
         ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+
+            /// Hot-swaps can be nested, so smaller updates can be made with
+            /// minimal performance impact
+            child: HotSwap(
+                id: '#myapp_fab_icon',
+                childBuilder: (context) => const Icon(Icons.add))),
       ),
-    );
-  }
-}
-
-class _LoadingIndicator extends StatelessWidget {
-  const _LoadingIndicator();
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(
-      height: 14,
-      width: 14,
-      child: CircularProgressIndicator(strokeWidth: 2),
     );
   }
 }
